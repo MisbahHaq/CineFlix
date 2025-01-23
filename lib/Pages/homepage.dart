6a5a80aa88b1030840ce612d8c24e56f9588bd8d
Misbah Haque <misbahmuneer2003@gmail.com>
2025-01-23 16:00:17 +0500

    Home Page Update

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -11,13 +11,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final List<String> imageUrls = [
-    "assets/images/damsel.jpg",
-    "assets/images/deadpool.jpg",
-    "assets/images/dune2.jpg",
-    "assets/images/infinity.jpg",
-    "assets/images/inter.jpg",
-    "assets/images/road.jpg",
-    "assets/images/zilla.jpg"
+    "images/damsel.jpg",
+    "images/deadpool.jpg",
+    "images/dune2.jpg",
+    "images/inter.jpg",
+    "images/road.jpg",
+    "images/zilla.jpg"
   ];
   @override
   Widget build(BuildContext context) {
@@ -25,79 +24,126 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.black,
       body: Container(
         margin: EdgeInsets.only(top: 40, left: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Text(
-                  'Hello, Misbah',
-                  style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold),
-                ),
-                Spacer(),
-                Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(60),
-                    child: Image.asset(
-                      "images/goku.jpg",
-                      height: 60,
-                      width: 60,
-                      fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Hello, Misbah',
+                    style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(60),
+                      child: Image.asset(
+                        "images/goku.jpg",
+                        height: 60,
+                        width: 60,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Text(
-              'Welcome To,',
-              style: GoogleFonts.poppins(
-                  color: Color.fromARGB(186, 255, 255, 255),
-                  fontSize: 18,
-                  fontWeight: FontWeight.normal),
-            ),
-            Row(
-              children: [
-                Text(
-                  "Cine",
-                  style: GoogleFonts.poppins(
-                    color: Colors.white,
-                    fontSize: 36,
-                    fontWeight: FontWeight.w500,
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Welcome To,',
+                style: GoogleFonts.poppins(
+                    color: Color.fromARGB(186, 255, 255, 255),
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal),
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Cine",
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 36,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
-                ),
-                Text(
-                  "Flix",
-                  style: GoogleFonts.poppins(
-                    color: Color(0xffedb41d),
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
+                  Text(
+                    "Flix",
+                    style: GoogleFonts.poppins(
+                      color: Color(0xffedb41d),
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Center(
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
                 child: CarouselSlider(
-                    items: imageUrls.map(
-                      (url) {
-                        return Builder(
-                          builder: ((context) {
-                            return Image.asset(
-                              url,
-                              fit: BoxFit.cover,
-                            );
-                          }),
-                        );
-                      },
-                    ).toList(),
-                    options: CarouselOptions()))
-          ],
+                  items: imageUrls.map(
+                    (url) {
+                      return Builder(
+                        builder: ((context) {
+                          return Container(
+                            width: MediaQuery.of(context).size.width,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.asset(
+                                url,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        }),
+                      );
+                    },
+                  ).toList(),
+                  options: CarouselOptions(
+                    height: 450,
+                    autoPlay: true,
+                    enlargeCenterPage: true,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.8,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Top Trending Movies",
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              ListView(
+                children: [
+                  Container(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          "images/damsel.jpg",
+                          height: 150,
+                          width: 150,
+                          fit: BoxFit.cover,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
