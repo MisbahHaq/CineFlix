336351fc72289b26170260be702891aa144f0464
Misbah Haque <misbahmuneer2003@gmail.com>
2025-01-23 12:10:26 +0500

    Updated

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,6 +10,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> imageUrls = [
+    "assets/images/damsel.jpg",
+    "assets/images/deadpool.jpg",
+    "assets/images/dune2.jpg",
+    "assets/images/infinity.jpg",
+    "assets/images/inter.jpg",
+    "assets/images/road.jpg",
+    "assets/images/zilla.jpg"
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,18 +26,77 @@ class _HomePageState extends State<HomePage> {
       body: Container(
         margin: EdgeInsets.only(top: 40, left: 20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               children: [
                 Text(
-                  'Hello',
+                  'Hello, Misbah',
                   style: GoogleFonts.poppins(
                       color: Colors.white,
-                      fontSize: 20,
+                      fontSize: 22,
                       fontWeight: FontWeight.bold),
-                )
+                ),
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(60),
+                    child: Image.asset(
+                      "images/goku.jpg",
+                      height: 60,
+                      width: 60,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
               ],
-            )
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              'Welcome To,',
+              style: GoogleFonts.poppins(
+                  color: Color.fromARGB(186, 255, 255, 255),
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal),
+            ),
+            Row(
+              children: [
+                Text(
+                  "Cine",
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  "Flix",
+                  style: GoogleFonts.poppins(
+                    color: Color(0xffedb41d),
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Center(
+                child: CarouselSlider(
+                    items: imageUrls.map(
+                      (url) {
+                        return Builder(
+                          builder: ((context) {
+                            return Image.asset(
+                              url,
+                              fit: BoxFit.cover,
+                            );
+                          }),
+                        );
+                      },
+                    ).toList(),
+                    options: CarouselOptions()))
           ],
         ),
       ),
